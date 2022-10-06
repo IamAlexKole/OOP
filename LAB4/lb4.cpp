4afc47388df585a36121ab50b2597813f37e919f
IamAlexKole <sasha2003kol@gmail.com>
2022-10-06 18:09:15 +0300

    Created lab4 and changed in CMakeLists

---
+++ LAB4/lb4.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+class Diamond {
+private:
+    float a, b;
+public:
+    Diamond() {
+        this->a = 0;
+        this->b = 0;
+    }
+    Diamond(float a, float b){
+        this->a = a;
+        this->b = b;
+    }
+    void setA(float a){
+        this->a = a;
+    }
+    void setB(float b){
+        this->b = b;
+    }
+    float getA(){
+        return a;
+    }
+    float getB(){
+        return b;
+    }
+    void print_sqrt() {
+        cout << "1st Diagonal = " << a << endl;
+        cout << "2nd Diagonal = " << b << endl;
+        cout << "Square = " << 0.5 * (a * b);
+    }
+};
+int main() {
+    Diamond diam(10, 15);
+    diam.print_sqrt();
+    return 0;
+}
